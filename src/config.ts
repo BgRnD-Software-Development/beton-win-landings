0400242acb0fa5671c61a06759d039de24fdcf6a
Anton Karpenko <karpolan@karpolan.com>
2025-01-10 12:30:26 +0100

    feat: REGISTRATION_URL env variable

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,8 @@ export const IS_PRODUCTION = getCurrentEnvironment() === 'production'; // Enable
 // export const PUBLIC_URL = envRequired(process.env.NEXT_PUBLIC_PUBLIC_URL); // Variant 1 - value is required
 export const PUBLIC_URL = process.env.NEXT_PUBLIC_PUBLIC_URL ?? 'http://localhost:3000'; // Variant 2 - value is optional
 
+export const REGISTRATION_URL = process.env.NEXT_PUBLIC_REGISTRATION_URL ?? 'http://localhost:3000';
+
 export const APP_NAME = 'Beton.win';
 export const APP_SHORT_NAME = 'BetOnWin';
 
